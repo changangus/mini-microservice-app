fd80ba3e077a15fda70038ae071dea738cfae844
Angus Chang <angus@heysparkbox.com>
2021-11-26 12:49:13 -0500

    feat: comments server
    - setup comments server
    
    MSU-2

---
+++ comments/index.js
@@ -0,0 +1,26 @@
+import express, { json } from 'express';
+import { randomBytes } from 'crypto';
+
+const app = express();
+
+app.use(json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  const postId = req.params.id;
+  res.send(commentsByPostId[postId] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { comment } = req.body;
+  const postId = req.params.id;
+  const comments = commentsByPostId[postId] || [];
+  commentsByPostId[postId] = [...comments, { commentId, comment }];
+  res.status(201).send(commentsByPostId[postId]);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on port 4001');
+});
